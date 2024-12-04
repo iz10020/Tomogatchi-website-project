ea0aabb2f37607761c5de5f2e1e66ae04092de32
arashg77 <arashiboy420@gmail.com>
2024-12-04 11:47:29 +0100

    router

---
+++ src/components/CharacterShop.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="shop-container">
+      <h2>Select Your Tamagotchi Character</h2>
+      <div class="character-options">
+        <div 
+          v-for="(image, index) in characterImages" 
+          :key="index" 
+          class="character-option"
+          @click="selectCharacter(image)"
+        >
+          <img :src="image" alt="Tamagotchi Character" class="character-image" />
+        </div>
+      </div>
+      <button @click="closeShop" class="close-shop">Close Shop</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      currentCharacter: String,
+    },
+    data() {
+      return {
+        characterImages: [
+          require('@/assets/kirbyidle.gif'),
+          require('@/assets/pengiunidle.gif'),
+          require('@/assets/catidle.gif'),
+          // Add more characters here
+        ],
+      };
+    },
+    methods: {
+      selectCharacter(image) {
+        this.$emit('character-selected', image);
+      },
+      closeShop() {
+        this.$emit('close-shop');
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .shop-container {
+    text-align: center;
+    padding: 20px;
+  }
+  
+  .character-options {
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+  }
+  
+  .character-option {
+    cursor: pointer;
+  }
+  
+  .character-image {
+    width: 50px;
+    height: 50px;
+    border-radius: 10px;
+    transition: transform 0.2s;
+  }
+  
+  .character-image:hover {
+    transform: scale(1.1);
+  }
+  
+  .close-shop {
+    margin-top: 10px;
+    padding: 10px;
+    background-color: #333;
+    color: white;
+    border: none;
+    cursor: pointer;
+  }
+  </style>
+  
